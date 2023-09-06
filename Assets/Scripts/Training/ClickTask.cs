f1d766bd7ec6ca0b972fd3aa633dae6968405fbb
Neto test <michaelGulidow@yandex.ru>
2023-09-06 16:02:53 +0300

    Fix click tasks

--- Assets/Scripts/Training/ClickTask.cs
+++ Assets/Scripts/Training/ClickTask.cs
@@ -4,14 +4,21 @@ using UnityEngine.UI;
 public class ClickTask : Task
 {
     [SerializeField] private Button _targetButton;
+    [SerializeField] private Button[] _excessButtons;
 
     protected override void BeginTask()
     {
+        foreach (var button in _excessButtons)
+            button.interactable = false;
+
         _targetButton.AddListener(Complete);
     }
 
     protected override void OnComplete()
     {
+        foreach (var button in _excessButtons)
+            button.interactable = false;
+
         _targetButton.RemoveListener(Complete);
         base.OnComplete();
     }
